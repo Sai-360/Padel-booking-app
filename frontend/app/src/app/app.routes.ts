0a972939e2fa64b5c46e785d0fecc509675c8c12
Saïdi <mohamed.saidi@student.vinci.be>
2026-05-05 05:29:53 +0200

    Add my reservations page

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import {Home} from './views/home/home';
 import {ReservationList} from './views/reservations/reservation-list/reservation-list';
 import {ReservationCreation} from './views/reservations/reservation-creation/reservation-creation';
 import {PublicMatches} from './views/public-matches/public-matches';
+import {MyReservations} from './views/my-reservations/my-reservations';
 export const routes: Routes = [
 
 
 {path: 'home', component: Home},
   { path: 'book', component: ReservationCreation },
-  { path: 'public-matches', component: PublicMatches }
-
+  { path: 'public-matches', component: PublicMatches },
+  { path: 'my-reservations', component: MyReservations }
 
 ];
--- frontend/app/src/app/views/my-reservations/my-reservations.ts
+++ frontend/app/src/app/views/my-reservations/my-reservations.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-my-reservations',
   imports: [],
   templateUrl: './my-reservations.html',
   styleUrl: './my-reservations.css',
+  standalone: true
 })
 export class MyReservations {
 
